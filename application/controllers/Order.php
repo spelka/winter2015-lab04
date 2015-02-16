6fd8d9e2643fe9293965b7ef432f1423eccb2081
spelka <sebastian.pelka@hotmail.com>
2015-02-15 16:17:52 -0800

    Submitting tutorial 4, validation is the only thing I cant figure out.
    Signed-off-by: spelka <sebastian.pelka@hotmail.com>

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -42,7 +42,8 @@ class Order extends Application {
         $this->data['order_num'] = $order_num;
         //FIXME
 		//get the order from the database
-		$order = $this->orders->get($order_num);
+		$order = $this->orders->get($order_num);		//order is the order object here
+		$this->data['title'] = "Order # " . $order->num . ": Total ($" . sprintf('%0.2f', $order->total) . ")";
 
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
@@ -84,6 +85,12 @@ class Order extends Application {
     // add an item to an order
     function add($order_num, $item) {
         //FIXME
+		//add an item
+		$this->orders->add_item($order_num, $item);
+		
+		//update the price
+		$this->orders->total($order_num);
+		
         redirect('/order/display_menu/' . $order_num);
     }
 
@@ -93,19 +100,58 @@ class Order extends Application {
         $this->data['pagebody'] = 'show_order';
         $this->data['order_num'] = $order_num;
         //FIXME
+		
+		$this -> data['total'] = number_format ($this -> orders -> total( $order_num ), 2);
+		
+		$items = $this -> orderitems -> group ( $order_num );
+		
+		foreach ( $items as $item )
+		{
+			$menuitem = $this -> menu -> get ( $item -> item );
+			$item -> code = $menuitem -> name;
+		}
+		
+		$this -> data['items'] = $items;
+		
+		//ensure one thing from each type is in the order
+		$this -> data['okornot'] = $this -> orders -> validate ( $order_num );
 
         $this->render();
     }
 
     // proceed with checkout
-    function proceed($order_num) {
+    function commit($order_num)
+	{
         //FIXME
+		if ( $this -> orders -> validate ( $order_num ) );
+		{
+			redirect ('/order/display_menu/' . $order_num);
+		}
+		//get the order entry in the RDB
+		$record  = $this -> orders -> get ( $order_num );
+		//set its date
+		$record -> date = date(DATE_ATOM);
+		//set its status to complete
+		$record -> status = 'c';
+		//set the order total for the record in the RDB
+		$record -> total = $this  -> orders -> total ( $order_num );
+		//update the RDB entry for this order
+		$this -> orders -> update ( $record );
         redirect('/');
     }
 
     // cancel the order
-    function cancel($order_num) {
+    function cancel($order_num)
+	{
         //FIXME
+		//delete items in our order
+		$this -> orderitems -> delete_some ( $order_num );
+		//get the order entry itself
+		$record = $this -> orders -> get ( $order_num );
+		//set status to cancelled
+		$record -> status = 'x';
+		//update the RDB
+		$this -> orders -> update ( $record );
         redirect('/');
     }
 
--- application/models/Orders.php
+++ application/models/Orders.php
@@ -10,33 +10,71 @@ class Orders extends MY_Model {
     // constructor
     function __construct() {
         parent::__construct('orders', 'num');
+		$CI = &get_instance();
+		$CI->load->model('orderitems');
+		$CI->load->model('menu');
     }
 
     // add an item to an order
-    function add_item($num, $code) {
-        
+    function add_item($num, $code)
+	{
+        $CI = get_instance();
+		//if the item is already part of the order
+		if ( $CI -> orderitems -> exists ( $num, $code ))
+		{
+			//retrieve the record, increment quantity, update table
+			$record = $CI -> orderitems -> get ( $num, $code);
+			$record -> quantity ++;
+			$CI -> orderitems -> update	( $record );
+		}
+		else
+		{
+			//else make an empty orderitem record, and populate its fields, qty = 1
+			$record = $CI -> orderitems -> create();
+			$record -> order = $num;
+			$record -> item = $code;
+			$record -> quantity = 1;
+			$CI -> orderitems -> add ( $record );
+		}
     }
 
     // calculate the total for an order
     function total($num) {
-		$order_total = 0.0;
-		//get the list of items from the orderitems database table
-		$orderItemArray = $this->orderitems->some( 'order' , $num );
 		
-		//iterate through the items and find the associated menu price
-		foreach($orderItemArray as $order)
+		//get all the items in order
+		$items = $this->orderitems->some('order', $num);
+		
+		//add them up
+		$result = 0.0;
+		foreach ($items as $item)
 		{
-			$item = $this->menu->get( 'code' , $order->item);
-			$order_total += ($item->price * $order->quantity);
+			$menuitem = $this->menu->get($item->item);
+			$result += $item->quantity * $menuitem->price;
 		}
+
+		//$order is a record from the Orders table given by key $num
+		$order = $this->orders->get($num);
+		//set the total in the record to the price calculated above
+		$order->total = $result;
+		//update the RDB record with the new information
+		$this->orders->update($order);
 		
-		//return the total
-        return money_format($order_total);
+		return $result;
     }
 
     // retrieve the details for an order
-    function details($num) {
-        
+    function details($num)
+	{
+        $details = $this -> orderitems -> some ( 'order' , $num );
+		
+		foreach ( $details as $entry )
+		{
+			$item = $this -> menu -> get ( $entry -> item);
+			$entry -> picture = $item -> picture;
+			$entry -> description = $item-> description;
+		}
+		
+		return $details;
     }
 
     // cancel an order
@@ -46,8 +84,26 @@ class Orders extends MY_Model {
 
     // validate an order
     // it must have at least one item from each category
-    function validate($num) {
-        return false;
+    function validate($num)
+	{
+		$CI = & get_instance();
+		$items = $CI ->orderitems -> group ( $num );
+		$gotem = array();
+		if ( count ( $items ) > 0 )
+		{
+			foreach ( $items as $item )
+			{
+				$menu = $CI -> menu -> get ( $item -> item );
+				$gotem [ $menu -> category ] = 1;
+			}
+		}
+		if (($gotem['m'] == 1) && ($gotem['d'] == 1) && ($gotem['s'] == 1))
+		{
+			return true;
+		}
+		else
+			return false;
+		
     }
 
 }
